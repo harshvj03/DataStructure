b5cbeb7b5fcc8d8415d46e79981fad3655a55145
Harsh <harshvj03@gmail.com>
2022-10-02 13:19:35 +0530

    add rec file

---
+++ rec.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int getMax(int arr[], int size, int index) {
+	if (index == size) {
+		return INT_MIN;
+	}
+
+	int ans = arr[index];
+
+}
+
+
+int main() {
+
+	UsingMap();
+	return 0;
+}
